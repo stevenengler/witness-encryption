acf82ba0171241de5c86cfc9d5566e3274d7428e
Steven Engler <smengler3@gmail.com>
2017-11-29 18:15:34 -0500

    Added first version of witness encryption interface.

---
+++ graded_witness_encryption.py
@@ -0,0 +1,51 @@
+def _format_key_string(key, bit_length):
+	sign_bit = (key[0] == '-')
+	if sign_bit:
+		key = key[1:]
+	#
+	key = str(int(sign_bit))+key
+	return key.zfill(bit_length)
+#
+def encrypt(mmap_instance, T):
+	n = mmap_instance.get_n()
+	lmda = mmap_instance.get_lambda()
+	#
+	a = [mmap_instance.sample() for x in range(n)]
+	a_prime = [mmap_instance.encode(1, a[x]) for x in range(n)]
+	ciphertext = []
+	#
+	for i in range(len(T)):
+		for j in range(len(a_prime)):
+			mmap_instance.rerand(1, a_prime[j])
+		#
+		this_T = T[i]
+		#
+		product = mmap_instance.copy_encoding(a_prime[this_T[0]])
+		for j in range(1, len(this_T)):
+			product = mmap_instance.multiply(product, a_prime[this_T[j]])
+		#
+		encoded = mmap_instance.encode(len(this_T), product)
+		ciphertext.append(encoded)
+	#
+	product = mmap_instance.copy_encoding(a_prime[0])
+	for i in range(1, n):
+		product = mmap_instance.multiply(product, a_prime[i])
+	#
+	encoding = mmap_instance.encode(n, product)
+	key = mmap_instance.extract(encoding)
+	key = _format_key_string(key, lmda)
+	#
+	return (key, ciphertext)
+#
+def decrypt(mmap_instance, ciphertext, W):
+	lmda = mmap_instance.get_lambda()
+	#
+	B = mmap_instance.copy_encoding(ciphertext[W[0]])
+	for i in range(1, len(W)):
+		B = mmap_instance.multiply(B, ciphertext[W[i]])
+	#
+	key_recovered = mmap_instance.extract(B)
+	key_recovered = _format_key_string(key_recovered, lmda)
+	#
+	return key_recovered
+#
---
+++ new_mmap_interface.py
@@ -0,0 +1,88 @@
+import mmap as mmap_lib
+# be careful here, mmap is already a built-in Python library name
+import graded_witness_encryption
+#
+class NewMultilinearMap:
+	def __init__(self, lmda, kappa, n, rho, etap):
+		self.mmap = mmap_lib.public_parameters_generate(lmda, kappa, n, rho, etap)
+		self.mmap.generate(False)
+	#
+	def get_n(self):
+		return self.mmap.params.n
+	#
+	def get_lambda(self):
+		return self.mmap.params._lambda
+	#
+	def copy_encoding(self, value):
+		encoded_value = mmap_lib.encoding(value)
+		return encoded_value
+	#
+	def sample(self):
+		samp = mmap_lib.encoding()
+		samp.samp(self.mmap, 0)
+		return samp
+	#
+	def encode(self, level, value):
+		return self.mmap.enc(value, level)
+	#
+	def rerand(self, level, value):
+		assert(level == 1)
+		value.rerand()
+	#
+	def multiply(self, a, b, store_in=None):
+		if store_in is None:
+			store_in = mmap_lib.encoding()
+		#
+		store_in.set_to(a)
+		store_in.mult_in_place(b)
+		return store_in
+	#
+	def extract(self, value):
+		return self.mmap.ext_as_str(value, 0)
+	#
+#
+if __name__=='__main__':
+	import ecigen
+	import math
+	#
+	lmda = 200
+	n = 10
+	#
+	lmda_mlm = lmda
+	n_mlm = 10
+	kappa_mlm = n
+	rho_mlm = 52
+	etap_mlm = 420
+	#
+	#T = [[0,2,4], [0,1], [3,5], [2], [1]]
+	#W = [0, 2, 4]
+	#
+	T, W = ecigen.generate(n, math.ceil(n/2), n*3)
+	T = [[y-1 for y in x] for x in T]
+	#
+	print('----------------------')
+	#
+	pp = NewMultilinearMap(lmda_mlm, kappa_mlm, n_mlm, rho_mlm, etap_mlm)
+	#
+	print('----------------------')
+	#
+	(K, C) = graded_witness_encryption.encrypt(pp, T)
+	#
+	print('K (first 20 bits): {0}...'.format(K[0:20]))
+	print('K length: {0}'.format(len(K)))
+	print('Num of 1 bits: {0}'.format(K.count('1')))
+	#
+	print('----------------------')
+	#
+	K_recovered = graded_witness_encryption.decrypt(pp, C, W)
+	#
+	print('K_recovered (first 20 bits): {0}...'.format(K_recovered[0:20]))
+	print('K_recovered length: {0}'.format(len(K_recovered)))
+	print('Num of 1 bits: {0}'.format(K_recovered.count('1')))
+	#
+	print('----------------------')
+	#
+	print('Keys are the same?: {0}'.format(K_recovered == K))
+	#
+	print('----------------------')
+	print()
