c9210c8fc02a30ba207674a1aaab378c81c02f95
Steven Engler <smengler3@gmail.com>
2017-12-19 19:43:13 -0500

    Moved 'test_ecigen.py' code.
    Moved the 'test_ecigen.py' code to 'ecigen.py' so it can be used as a script or a library.

--- ecigen.py
+++ ecigen.py
@@ -41,3 +41,29 @@ def generate(n, l, t):
         witness_indices.append(index)
 
     return (subsets, sorted(witness_indices))
+
+if __name__=='__main__':
+    import itertools
+    import sys
+
+    # run this code to test the exact cover instance generation. Ex: `python ecigen.py 10 5 20`
+
+    n = int(sys.argv[1])
+    l = int(sys.argv[2])
+    t = int(sys.argv[3])
+    subsets, witness = generate(n, l, t)
+
+    # Check that we have the right number of subsets
+    assert(len(subsets) == t)
+    # Check witness is the right length
+    assert(len(witness) == l)
+
+    witness_subsets = [subsets[i] for i in witness]
+
+    # Check that we have exactly n elements across all witness subsets
+    assert(sum([len(i) for i in witness_subsets]) == n)
+    # Check that their union is the {1, 2, ..., n}
+    assert(set(itertools.chain.from_iterable(witness_subsets)) == set(range(1, n+1)))
+
+    print(subsets)
+    print(witness)
